cdf161aed9b773d72427b17e46594db065daa094
Pedro Henrique <pedrohs343@gmail.com>
2024-06-14 11:43:43 -0300

    First commit

---
+++ main.py
@@ -0,0 +1,64 @@
+import os.path
+import requests
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
+
+SAMPLE_SPREADSHEET_ID = ""
+SAMPLE_RANGE_NAME = "A1:Z"
+
+def main():
+  """Shows basic usage of the Sheets API.
+  Prints values from a sample spreadsheet.
+  """
+  creds = None
+  
+  if os.path.exists("token.json"):
+    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+
+  if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+      creds.refresh(Request())
+    else:
+      flow = InstalledAppFlow.from_client_secrets_file(
+          "credentials.json", SCOPES
+      )
+      creds = flow.run_local_server(port=0)
+
+    with open("token.json", "w") as token:
+      token.write(creds.to_json())
+
+  try:
+    service = build("sheets", "v4", credentials=creds)
+    list_values = []
+    print('Digite seu cep:')
+    cep = input().replace("-","").replace(" ", "")
+    request = requests.get(f"https://viacep.com.br/ws/{cep}/json/")
+
+    if request.status_code == 200:
+      print("Cep cadastrado com sucesso!")
+      dados = request.json()
+      list_values.append([dados['cep'], dados['logradouro'],dados['bairro'], dados['localidade']])
+
+    sheet = service.spreadsheets()
+    result = (
+        service.spreadsheets()
+        .values()
+        .append(
+            spreadsheetId=SAMPLE_SPREADSHEET_ID,
+            range=SAMPLE_RANGE_NAME,
+            valueInputOption='RAW',
+            body={"values":list_values},
+        )
+        .execute()
+    )
+   
+  except HttpError as err:
+    print(err)
+
+if __name__ == "__main__":
+  main()
